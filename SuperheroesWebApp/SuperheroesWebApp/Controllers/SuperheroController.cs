b9b68c469fbef5e02530f69052d7ece59eeb719e
larutow <lucas.rutowski@gmail.com>
2020-11-11 14:05:19 -0600

    added superhero model, migrated and updated db with superhero object, made SuperheroController, added logic to create method and index, added views for create and index

---
+++ SuperheroesWebApp/SuperheroesWebApp/Controllers/SuperheroController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SuperheroesWebApp.Data;
+using SuperheroesWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroesWebApp.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        private ApplicationDbContext _context;
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: HeroController
+        public ActionResult Index()
+        {
+            return View(_context.Superheroes);
+        }
+
+        // GET: HeroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HeroController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HeroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero hero)
+        {
+            try
+            {
+                _context.Superheroes.Add(hero);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HeroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HeroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HeroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HeroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
